2dbf57c16aa8844ddde509069ffb8ce4d0c303c1
Priyanka Lohray <mahalepri@gmail.com>
2015-07-02 00:07:01 -0700

    Testing Weather table - Android test fails

--- app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDbHelper.java
@@ -33,7 +33,7 @@ package com.example.android.sunshine.data;
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     static final String DATABASE_NAME = "weather.db";
 
